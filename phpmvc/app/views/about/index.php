b0c2db21beff514e685e1fef2e78dea86408ac43
rayzaarf <rayza.arfian@gmail.com>
2019-02-25 14:44:44 +0700

    Add files via upload

---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/profile.jpg" alt="" width="150" class="rounded-circle">
+    <p>Halo, nama saya <?= $data['nama'] ?> dan saya adalah <?= $data['pekerjaan'] ?></p>
+</div>
